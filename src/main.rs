e4d7128ffc31b47d7c5f3e04fd6120bf20f5fe2b
mtoon <77199166+mtoons@users.noreply.github.com>
2025-01-11 17:52:05 +0100

    copied the start of a parsor from somewhere will delete i just need to sync

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod text_tables;
 use std::env;
 //use text_tables::text_tables::{expand_math_shortcut, name_to_symbol};
+#[derive(Debug, Clone)]
+pub enum GrammarItem {
+    Paren,
+    Parentheses(u8),
+    Operation(u16),
+    Number(u64),
+    Literal(String)
+    Character(char)
+    Function(String)
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseNode {
+    pub children: Vec<ParseNode>,
+    pub entry: GrammarItem,
+}
+impl ParseNode {
+    pub fn new() -> ParseNode {
+        ParseNode {
+            children: Vec::new(),
+            entry: GrammarItem::Paren,
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();

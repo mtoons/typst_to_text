1e9902d99b556565065770df0a63f26d868226b5
mtoon <77199166+mtoons@users.noreply.github.com>
2025-01-12 16:04:06 +0100

    ok we can do single file module that's great

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
 mod text_tables;
 use std::env;
-//use text_tables::text_tables::{expand_math_shortcut, name_to_symbol};
-#[derive(Debug, Clone)]
+use text_tables::{subscript, superscript};
 pub enum GrammarItem {
-    Paren,
-    Parentheses(u8),
-    Operation(u16),
-    Number(u64),
-    Literal(String)
-    Character(char)
-    Function(String)
+    Parentheses(Vec<GrammarItem>),
+    Symbol(String),
+    Number(f64),
+    Literal(String),
+    Character(char),
+    Subscript(Vec<GrammarItem>),
+    Superscript(Vec<GrammarItem>),
 }
 
-#[derive(Debug, Clone)]
-pub struct ParseNode {
-    pub children: Vec<ParseNode>,
-    pub entry: GrammarItem,
-}
-impl ParseNode {
-    pub fn new() -> ParseNode {
-        ParseNode {
-            children: Vec::new(),
-            entry: GrammarItem::Paren,
-        }
+// maybe switch to range
+fn render(typst: Vec<GrammarItem>) -> String {
+    let mut text: String = "".to_string();
+    for e in typst {
+        match e {
+            GrammarItem::Parentheses(content) => {
+                text.push('(');
+                text.push_str(&render(content));
+                text.push(')');
+            }
+            GrammarItem::Symbol(symbol) => {
+                text.push_str(&symbol);
+            }
+            GrammarItem::Number(number) => {
+                text.push_str(&number.to_string());
+            }
+            GrammarItem::Literal(literal) => {
+                text.push_str(&literal);
+            }
+            GrammarItem::Character(char) => {
+                text.push(char);
+            }
+            GrammarItem::Subscript(content) => {
+                text.push_str(&subscript(render(content)));
+            }
+            GrammarItem::Superscript(content) => {
+                text.push_str(&superscript(render(content)));
+            }
+        };
     }
+    text
+}
+
+pub fn parse(typst: String) -> Vec<GrammarItem> {
+    todo!()
 }
 
 fn main() {
@@ -33,6 +55,17 @@ fn main() {
         input.push(' ');
         input.push_str(&elem);
     }
-    let output: String = input;
+    let nodes = parse(input);
+    let output = render(nodes);
     print!("{}", output);
+}
+
+#[test]
+fn render_test() {
+    let nodes: Vec<GrammarItem> = vec![
+        GrammarItem::Character('a'),
+        GrammarItem::Subscript(vec![GrammarItem::Literal("text".to_string())]),
+    ];
+    // print!("{}", render(nodes));
+    assert_eq!(render(nodes), "aₜₑₓₜ")
 }
